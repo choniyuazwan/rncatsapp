bda22fe106f5d05ec3c51d917fa42ac6c30f83d2
Choniyu Azwan <choniyuazwan@gmail.com>
2021-09-10 07:55:52 +0700

    add search function

--- App.js
+++ App.js
@@ -1,50 +1,64 @@
 import React, { Component } from 'react';
 import { StyleSheet, FlatList, View, Text, ActivityIndicator } from 'react-native';
 import {Accordion, Box, Center, NativeBaseProvider, IconButton, Input} from "native-base";
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://api.thecatapi.com/v1';
+axios.defaults.headers.common['x-api-key'] = 'DEMO-API-KEY';
+
 export default class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
       data: [],
       page: 1,
-      isLoading: false
+      isLoading: true,
+      search: '',
+      isSearch: false
     }
   }
   componentDidMount() {
-    this.setState({isLoading: true}, this.getData)
+    console.log('componentDidMount called')
+    this.loadData();
   }
 
-  getData = async () => {
-    const apiURL = `https://api.thecatapi.com/v1/breeds?limit=10&page=${this.state.page}`;
-
-    fetch(apiURL, {
-      headers: {"x-api-key": "DEMO-API-KEY"}
-    }).then((res) => res.json())
-      .then((resJson) => {
+  loadData = () => {
+    this.setState({isLoading: true})
+    const { page, data, isSearch } = this.state;
+    const url = `/breeds?limit=10&page=${page}`;
+    !isSearch && axios
+    // axios
+      .get(url)
+      .then((res) => {
+        let result = res.data;
         this.setState({
-          data: this.state.data.concat(resJson),
-          isLoading: false
-        })
+          data: data.concat(result),
+          page: page + 1,
+          isLoading: false,
+        });
       })
-  }
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
   accordionComponent = ({item}) => {
     return (
-      <Box>
-        <Accordion>
-          <Accordion.Item>
-            <Accordion.Summary>
-              <Text>{ item.name }</Text>
-              <Accordion.Icon />
-            </Accordion.Summary>
-            <Accordion.Details>
-              <Text>Country : {item.origin}</Text>
-              <Text>{item.description}</Text>
-              <Text>Temprament : {item.temperament}</Text>
-            </Accordion.Details>
-          </Accordion.Item>
-        </Accordion>
-      </Box>
+      // <Box>
+      <Accordion>
+        <Accordion.Item>
+          <Accordion.Summary>
+            <Text>{ item.name }</Text>
+            <Accordion.Icon />
+          </Accordion.Summary>
+          <Accordion.Details>
+            <Text>Country : {item.origin}</Text>
+            <Text>{item.description}</Text>
+            <Text>Temprament : {item.temperament}</Text>
+          </Accordion.Details>
+        </Accordion.Item>
+      </Accordion>
+      // </Box>
     );
   }
 
@@ -56,25 +70,45 @@ export default class App extends Component {
         </View> : null
     )
   }
-
-  handleLoadMore = () => {
-    console.log('load more called')
-    this.setState({ page: this.state.page + 1, isLoading: true }, this.getData)
-  }
+  
+  searchData = () => {
+    this.setState({isLoading: true})
+    axios.get(`/breeds/search?q=${this.state.search}`).then((res) => {
+      let result = res.data;
+      this.setState({ isSearch: true, data: result, isLoading: false });
+    });
+  };
 
   _onChangeSearchText(text) {
-
-    console.log(text);
+    this.setState(
+      {
+        search: text,
+        page: 0,
+      },
+      () => {
+        if (this.state.search && this.state.search.length > 0) {
+          console.log('search data')
+          this.searchData();
+        } else {
+          console.log('get data')
+          this.loadData();
+        }
+        console.log('isi text', text);
+        console.log('panjang text', this.state.search.length);
+        console.log('panjang data', this.state.data.length);
+      }
+    );
 
   }
 
   render() {
+    const {data} = this.state;
     return (
       <NativeBaseProvider>
-        <Center flex={1}>
+        <Center>
           
           <Input
-            placeholder="Search..."
+            placeholder="Search cat name here ..."
             variant="rounded"
             InputRightElement={
               <IconButton
@@ -83,13 +117,12 @@ export default class App extends Component {
             }
             onChangeText={this._onChangeSearchText.bind(this)}
           />
-           
           <FlatList
             style={styles.container}
-            data={this.state.data}
+            data={data}
             renderItem={this.accordionComponent}
             keyExtractor={(item, index) => index.toString()}
-            onEndReached={this.handleLoadMore}
+            onEndReached={this.loadData}
             onEndReachedThreshold={1}
             ListFooterComponent={this.renderFooter}
           />
